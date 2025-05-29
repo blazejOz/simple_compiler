dcdc597135525a6e8fd94ffbb6eb25ee2fd03ada
blazejOz <blazej.ozog@gmail.com>
2025-05-29 10:22:17 +0200

    start to work on asm code genereator

---
+++ code_generator/asm_generator.py
@@ -0,0 +1,15 @@
+
+class AsmGenreator():
+    """
+    Generate assembly code using libc library
+    """
+    def __init__(self, ir_list):
+        self.ir_list = ir_list
+        self.asm = []
+
+    
+    def gen(self):
+        pass
+
+    def gen_header():
+        pass
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from syntax_analysis.parser import Parser
 from intermediate_representation.ir_generator import IRGenerator
 
 
-src    = "print(1+2*3) \n print(3)"
+src    = "print(1+2*3)"
 tokens = Lexer(src).tokenize()
 ast    = Parser(tokens).parse()
-print(ast)
-#ir_instr = IRGenerator(ast).gen()
+ir_instr = IRGenerator(ast).gen()
 
-#print(ir_instr)
+for innstr in ir_instr:
+    print(innstr)
