89b08ed63c15e8e4ff84ce73d5975ecff6056e32
blazejOz <blazej.ozog@gmail.com>
2025-05-29 09:39:22 +0200

    first draft of IRGenerator

--- intermediate_representation/ir_generator.py
+++ intermediate_representation/ir_generator.py
@@ -1,7 +1,4 @@
-# code_generator/ir_generator.py
-# Skeleton for IR generation
-
-from ast_classes import NumberExpr, PrintStmt
+from ast_classes import NumberExpr, PrintStmt, BinaryExpr
 
 class IRInstr:
     def __init__(self, op, arg1=None, arg2=None, res=None):
@@ -13,10 +10,59 @@ class IRInstr:
         return f"{self.res or ''} = {self.op} {self.arg1 or ''} {self.arg2 or ''}" if self.res else f"{self.op} {self.arg1 or ''} {self.arg2 or ''}"
 
 class IRGenerator:
+    """
+    
+    """
     def __init__(self, asts):
         self.asts = asts
         self.temp_id  = 0
         self.label_id = 0
-        self.instrs = []
+        self.ir_list = []
 
+    def new_temp(self):
+        """generete new temporrary variable"""
+        self.temp_id += 1
+        return "t" + str(self.temp_id)
+
+    def new_label(self, name="L"):
+        """ genereate new label"""
+        self.label_id += 1 
+        return name + str(self.label_id)
+
+    def gen(self):
+        for node in self.asts:
+            if isinstance(node, PrintStmt):
+                self.gen_print(node)
+            # elif isinstance(node, NumberExpr):
+                # tmp = self.gen_expr(node)
+            else:
+                raise NotImplementedError(f"Nieobslugiwany AST: {type(node)}")
+        return self.ir_list
     
+    def gen_print(self, node):
+        tmp = self.gen_expr(node.expr) #generete IR for expr - print(expr)
+
+        #print parmas:
+        self.ir_list.append(IRInstr('param', "fmt")) # fmt - format param for printf
+        self.ir_list.append(IRInstr('param', tmp)) # param of what to print
+        #call print:
+        call_tmp = self.new_label("call")
+        self.ir_list.append(IRInstr('call', 'printf', None, call_tmp))
+
+
+    def gen_expr(self, node):
+        if isinstance(node, NumberExpr):
+            temp  = self.new_temp()
+            self.ir_list.append(IRInstr('const', node.value, None, temp))
+            return temp
+        if isinstance(node, BinaryExpr):
+            left = self.gen_expr(node.left)
+            right = self.gen_expr(node.right)
+            res = self.new_temp()
+            operator_map = {'+':'add', '-':'sub', '*':'mul', '/':'div'}
+            operator = operator_map[node.op]
+            self.ir_list.append(IRInstr(operator, left, right, res))
+            return res
+        
+        raise NotImplementedError(f"IR gen for {type(node)} error")
+            
