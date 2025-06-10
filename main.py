8cc8d99450aad4120498b81b26039b13e20822ee
blazejOz <blazej.ozog@gmail.com>
2025-06-10 08:39:33 +0200

    main now produces executable from asm code

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from code_generator.asm_generator import AsmGenreator
+import subprocess
+from code_generator.asm_generator import AsmGenerator
 from lexical_analysis.lexer import Lexer
 from syntax_analysis.parser import Parser
 from intermediate_representation.ir_generator import IRGenerator
 
 
-src    = "print(123)"
+src    = "print(1+3*2)"
 tokens = Lexer(src).tokenize()
 # print(tokens)
 ast    = Parser(tokens).parse()
@@ -14,5 +15,12 @@ ir_instr = IRGenerator(ast).gen()
 for innstr in ir_instr:
     print(innstr)
 
-asm = AsmGenreator(ir_instr).gen()
+asm = AsmGenerator(ir_instr).gen()
 print(asm)
+
+with open("out.asm", "w") as f:
+    f.write(asm)
+
+subprocess.run(["nasm", "-felf64", "out.asm", "-o", "out.o"], check=True)
+subprocess.run(["gcc", "out.o", "-no-pie", "-o", "a.out"], check=True)
+print("Executable 'a.out' generated.")
